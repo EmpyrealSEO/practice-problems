4525eaf4150ce85dd9abc63354b60eb9c8b72bf9
Dustin Gomez <dustinbgomez@gmail.com>
2019-02-09 16:13:52 -0800

    start

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,8 +1,12 @@
 
-function sumArray(){
-	
+var numbers = [1,5,3,2,6];
+function sumArray(total, num){
+	return total + num;
 }
-
+function myFunction(item) {
+    document.getElementById("demo").innerHTML = numbers.reduce(getSum);
+  } 
+  
 function fitWithinVal(){
 
 }
